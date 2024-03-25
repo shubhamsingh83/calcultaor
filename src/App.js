5e4039597852391b7cd803f54365ed10e3d9ba8a
shubhamsingh83 <akashsingh832001@gmail.com>
2024-03-26 02:51:01 +0530

    cal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const[input,setInput] = useState("");
+  const[output,setOutput]=useState("");
+
+  const addToInput =(value) =>{
+            setInput((prevInput)=>prevInput + value)
+  };
+
+  const clearInput =()=>{
+     setInput("");
+     setOutput("");
+  }
+
+  const calculate = ()=>{
+    try{
+      if(input.includes("/0")){
+        setOutput("Infinity");
+        return;
+      }
+
+      if(input.trim === ""){
+        setOutput("Error");
+        return;
+      }
+
+      const result = eval(input);
+      if(isNaN(result)){
+        setOutput("NaN");
+      }
+      else{
+        setOutput(result);
+      }
+    }
+    
+    catch(error){
+       setOutput("Error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <input type="text" value={input} readOnly/>
+      <div className="result">{output}</div>
+       <div className="buttons">
+        <button onClick={()=>addToInput("7")}>7</button>
+        <button onClick={() => addToInput("8")}>8</button>
+        <button onClick={() => addToInput("9")}>9</button>
+        <button onClick={() => addToInput("+")}>+</button>
+
+        <button onClick={() => addToInput("4")}>4</button>
+        <button onClick={() => addToInput("5")}>5</button>
+        <button onClick={() => addToInput("6")}>6</button>
+        <button onClick={() => addToInput("-")}>-</button>
+
+        
+        <button onClick={() => addToInput("1")}>1</button>
+        <button onClick={() => addToInput("2")}>2</button>
+        <button onClick={() => addToInput("3")}>3</button>
+        <button onClick={() => addToInput("*")}>*</button>
+
+        <button onClick={clearInput}>C</button>
+        <button onClick={() => addToInput("0")}>0</button>
+        <button onClick={calculate}>=</button>
+        <button onClick={() => addToInput("/")}>/</button>
+       </div>
     </div>
   );
 }
